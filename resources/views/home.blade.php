d1219fe5baef497f0430c01ba0e394c024069175
sazzad7299 <sazzadurrahman580@gmail.com>
2022-01-14 22:18:44 +0600

    modified slider and user dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,31 @@
-@extends('layouts.app')
+@extends('layouts.front_master')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+<div class="dashboard " style="background:black;padding-top:70px">
+</div>
+    <div class="container">
+        <!-- Coundown Section Start -->
+        <section class="countdown-timer section-padding">
+            <div class="container">
+            <div class="row text-center">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="heading-count">
+                    <h2 class="wow fadeInDown" data-wow-delay="0.2s">Hi {{ Auth::user()->name }}</h2>
+                </div>
+                </div>
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="row time-countdown justify-content-center wow fadeInUp" data-wow-delay="0.2s">
+                    <div id="item" class="time-count">
+                        <div class="time-entry days"><span>0</span>Completed Event</div>
+                        <div class="time-entry hours"><span>00</span> On Going</div>
+                        <div class="time-entry minutes"><span>00</span> Upcomming</div>
+                        <div class="time-entry seconds"><span><i class="lni lni-user"></i></span> Profile</div> </div>
+                </div>
+                <a href="pricing.html" class="btn btn-common wow fadeInUp" data-wow-delay="0.3s">Add to My Calender</a>
                 </div>
             </div>
-        </div>
+            </div>
+        </section>
+        <!-- Coundown Section End -->
     </div>
-</div>
 @endsection
